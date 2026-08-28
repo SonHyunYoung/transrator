cee1abab59c5d6ac7480006381feef8e516e59d3
SonHyunYoung <son2497474@gmail.com>
2026-08-28 12:41:43 +0900

    feat : setup Db connect

--- backend/maria.js
+++ backend/maria.js
@@ -1,1 +1,13 @@
-//db 셋팅 관련 파일
+//db 연결 
+require("dotenv").config(); //env 파일 연결
+const db = require("mysql"); //db연결 모듈
+
+const conn = db.createConnection({
+    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,
+    user : process.env.DB_USER,
+    password : process.env.DB_PW,
+    database : process.env.DB_NAME
+})
+
+module.export = conn;
