88088765b78bb92ccc9d251f428d50545ed5d599
SonHyunYoung <son2497474@gmail.com>
2026-08-28 13:06:38 +0900

    fix : auth.js 오타 수정

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,12 +6,12 @@ const jwt = require("jsonwebtoken");
 const AuthMiddleWare = (req, res, next) => {
 
     //헤더에서 토큰 가져옴
-    const authHeader = req.header[authHeader];
+    const authHeader = req.headers[authHeader];
     const token = authHeader && authHeader.split(" ")[1];
 
     //토큰이 존재하지 않을 시 로그인 필요함 안내
     if(!token){
-        res.status(400).json({
+        return res.status(401).json({
             message : "앱 이용을 위해서는 로그인이 필요합니다."
         });
     }
@@ -23,7 +23,7 @@ const AuthMiddleWare = (req, res, next) => {
 
         next();
     } catch(err){
-        res.status(400).json({
+        res.status(401).json({
             message : "유효하지 않은 토큰입니다. 다시 로그인해주세요."
         });
     }
